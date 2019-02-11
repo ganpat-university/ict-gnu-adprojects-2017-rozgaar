052390b517bfa8558b336795ffb63837821a17eb
RC2801 <38161862+RC2801@users.noreply.github.com>
2019-02-11 09:17:46 +0530

    Add files via upload

--- projects/registrationOption.php
+++ projects/registrationOption.php
@@ -11,7 +11,18 @@
 		<div class="s3">
 		<p>&nbsp; Here you can select what occupation you want to go for.Three options are given and each 
 		has some common and some unique functionalities.Choose according to your
-		preference and need<br><br>
+		preference and need<br><br></div>
+		<div style="margin-left:480px;" class="sel">
+			<div class="s2"><b>Rozgaar</b> Occupation</div>
+			<br/>
+			<div class="s1"><a href="registerComp.php">Company</a></div>
+			</br>
+			<div class="s1"><a href="registrationIndi.php">Individual</a></div>
+			</br>
+			<div class="s1"><a href="registerEmp.php">Employee</a></div>
+		</div>
+		<div class="s3" style="margin-top:17%;"> 
+		<p>
 			<b>&nbsp;Company:</b> If you have your own organization or you represent the organization you 
 			work for,then you can select this option which will provide a platform for your organization
 			to hire people who are eligible and want to work on big projects.(Can only post jobs)<br><br>
@@ -23,14 +34,5 @@
 			gain experience of different type of projects.(Can only accept jobs)
 		</p>
 		</div>
-		<div style="margin-top:50px;margin-left:480px;" class="sel">
-			<div class="s2"><b>Rozgaar</b> Occupation</div>
-			<br/>
-			<div class="s1"><a href="registerComp.php">Company</a></div>
-			</br>
-			<div class="s1"><a href="registrationIndi.php">Individual</a></div>
-			</br>
-			<div class="s1"><a href="registerEmp.php">Employee</a></div>
-		<div>
 	</body>
 </html>
